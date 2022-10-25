6c4cee869e54647402c3a1e4f7d0af722405918d
guidesify <64188976+guidesify@users.noreply.github.com>
2022-10-25 17:54:52 +0800

    Update graph.R

--- graph.R
+++ graph.R
@@ -20,11 +20,17 @@ dengue$TotalCasesinCluster <- as.numeric(dengue$TotalCasesinCluster)
 dengue$ClusterNumber <- as.numeric(dengue$ClusterNumber)
 dengue$StreetAddress <- str_to_title(dengue$StreetAddress)
 
-# Make a column clusterno-date
-dengue$clusterno_date <- paste(dengue$ClusterNumber, dengue$Date, sep = "-")
-# Remove duplicates from the data
-dengue <- dengue[!duplicated(dengue$clusterno_date),]
+# # Make a column clusterno-date
+# dengue$clusterno_date <- paste(dengue$ClusterNumber, dengue$Date, sep = "-")
+# # Remove duplicates from the data
+# dengue <- dengue[!duplicated(dengue$clusterno_date),]
 str(dengue)
+
+#Read population data
+pop <- read_csv("Data/population_2019.csv")
+pop <- pop[, c("Planning Area", "total")]
+names(pop) <- c("PlanningArea", "Population")
+str(pop)
 
 ui <- fluidPage(
 
@@ -92,7 +98,7 @@ server <- function(input, output, session) {
             filter(if (input$filterby == "Subzone") {
                 Subzone == input$subzone | input$subzone == "ALL"
             } else if (input$filterby == "Planning Area") {
-                PlanningArea == input$planningarea | input$planningarea == "ALL"
+                PlanningArea == input$planningarea | input$planningarea == "ALL"                
             } else if (input$filterby == "Region") {
                 Region == input$region | input$region == "ALL"
             } else if (input$filterby == "Land Use") {
@@ -101,14 +107,28 @@ server <- function(input, output, session) {
                 grepl(input$location, StreetAddress, ignore.case = TRUE) | input$location == ""
             })
     })
+
+    pop_filtered <- reactive({
+        pop %>% 
+        filter(PlanningArea == input$planningarea | input$planningarea == "ALL")
+    })
     
     # Plot graph based on selection and sum up the cases by Date, set tooltip to text
     output$plot1 <- renderPlotly({
+
+        population_if_planning_area <-
+            if (input$filterby == "Planning Area") { 
+                "<br>Population: " %>% 
+                    paste(scales::comma(sum(pop_filtered()$Population)))
+            } else {
+                ""
+            }
+            
         if (input$show == "Recent Cases") {
             p <- dengue_filtered() %>%
                 group_by(Date) %>%
                 summarise(RecentCasesinCluster = sum(RecentCasesinCluster)) %>%
-                ggplot(., aes(x = Date, y = RecentCasesinCluster, text = paste("Date:", Date, "<br>Recent Cases:", RecentCasesinCluster))) +
+                ggplot(., aes(x = Date, y = RecentCasesinCluster, text = paste("Date:", Date, "<br>Recent Cases:", scales::comma(RecentCasesinCluster), population_if_planning_area))) +
                 geom_bar(stat = "identity", fill = "#6e2a25", na.rm = TRUE) +
                 labs(x = "Date", y = "Number of Cases", title = "Recent Dengue Cases") +
                 theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
@@ -118,7 +138,7 @@ server <- function(input, output, session) {
             p <- dengue_filtered() %>%
                 group_by(Date) %>%
                 summarise(TotalCasesinCluster = sum(TotalCasesinCluster)) %>%
-                ggplot(., aes(x = Date, y = TotalCasesinCluster, text = paste("Date:", Date, "<br>Total Cases:", TotalCasesinCluster))) +
+                ggplot(., aes(x = Date, y = TotalCasesinCluster, text = paste("Date:", Date, "<br>Total Cases:", scales::comma(TotalCasesinCluster), population_if_planning_area))) +
                 geom_bar(stat = "identity", fill = "#2e0303", na.rm = TRUE) +
                 labs(x = "Date", y = "Number of Cases", title = "Total Dengue Cases") +
                 theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
@@ -128,15 +148,16 @@ server <- function(input, output, session) {
             p <- dengue_filtered() %>%
                 group_by(Date) %>%
                 summarise(ActiveClusters = n_distinct(ClusterNumber)) %>%
-                ggplot(., aes(x = Date, y = ActiveClusters, text = paste("Date:", Date, "<br>No. of Active Clusters:", ActiveClusters))) +
+                ggplot(., aes(x = Date, y = ActiveClusters, text = paste("Date:", Date, "<br>No. of Active Clusters:", scales::comma(ActiveClusters), population_if_planning_area))) +
                 geom_bar(stat = "identity", fill = "#ca7b04", na.rm = TRUE) +
                 labs(x = "Date", y = "Number of Active Clusters", title = "Number of Active Clusters") +
                 theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
                 scale_x_date(date_breaks = "1 month", date_labels = "%b %Y") +
                 theme(plot.title = element_text(hjust = 0.5))
-        }
-        ggplotly(p, tooltip = c("text"))
+        } 
+        ggplotly(p, tooltip = c("text", "label"))
     })
 }
 
 shinyApp(ui, server)
+
